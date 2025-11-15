d52c08e819ee7329471fb0166fd1ed5025f93494
thinhthq220799 <thinhthq220799@tvu-onschool.edu.vn>
2025-11-15 10:07:05 +0700

    Tạo Project

---
+++ SHOPGIAY/SHOPGIAY/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SHOPGIAY.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
